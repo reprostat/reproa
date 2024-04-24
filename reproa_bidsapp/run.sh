ff28298bcb0e44e060b95ad2d57d614a3c57775c
tiborauer <tibor.auer@gmail.com>
2024-04-24 15:55:11 +0000

    FIX - config not found

--- reproa_bidsapp/run.sh
+++ reproa_bidsapp/run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-octave ${TOOLS_DIR}/reproanalysis/reproa_bidsapp.m $@
+# Config
+CONFIGDIR=$HOME/.reproa
+
+if [[ ! -d $CONFIGDIR ]]; then mkdir $CONFIGDIR; fi
+if [[ -e $CONFIGDIR/reproa_parameters_user.xml ]]; then
+    BCP="$CONFIGDIR/bcp-$(date +"%Y%m%d%H%M%S")_reproa_parameters_user.xml"
+    echo "Configfile $CONFIGDIR/reproa_parameters_user.xml already exists, and it may not correspond to this installation!"
+    echo "Existing configfile will be backed up as $BCP"
+    echo "You may want to restore it after this execution"
+    mv $CONFIGDIR/reproa_parameters_user.xml $BCP
+fi
+cp ${TOOLS_DIR}/reproa_bidsapp/parameters.xml $CONFIGDIR/reproa_parameters_user.xml
+
+# Run
+octave ${TOOLS_DIR}/reproa_bidsapp/reproa_bidsapp.m $@
