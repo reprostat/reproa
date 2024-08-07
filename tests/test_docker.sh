d9b51339bc42c7c9d66475da051321de44df73fb
tiborauer <tibor.auer@gmail.com>
2024-08-07 15:29:09 +0000

    TEST/CI

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -1,7 +1,7 @@
 docker run -ti --rm \
 	--user $(id -u):$(id -g) --env="DISPLAY" --env="HOME" --env="XDG_RUNTIME_DIR" --volume="$HOME:$HOME:rw" --volume="/dev:/dev:ro" --volume="${XDG_RUNTIME_DIR}:${XDG_RUNTIME_DIR}:rw" \
 	--network=host \
-    --volume=/ceph/users/usq33871/data/ds000114:/data \
+    --volume=/ceph/users/usq33871/data/ds114_test1:/data \
     --volume=/ceph/users/usq33871/projects/test_docker:/outputs \
 	--volume=/ceph/users/usq33871/projects/reproa/tests:/config \
-	reproa:dev /data /outputs participant --participant_label sub-01 --config /config/ds114_test1.xml:/config/ds114_test1.m --skip_bids_validator
+	reproa:dev /data /outputs participant --participant_label 01,02 --config /config/ds114_test1.xml,/config/ds114_test1.m --skip_bids_validator
